bb4ab5378566b13a9d480b6f06649e33394225ce
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2020-11-26 21:17:26 -0300

    first commit
    primeira aula com exercícios

---
+++ desafio-01.py
@@ -0,0 +1,5 @@
+print('========== DESAFIO 01 ==========')
+
+nome = input('Qual é seu nome? ')
+
+print('Bem vindo(a),', nome,'!')
---
+++ desafio-03.py
@@ -0,0 +1,7 @@
+print('========== DESAFIO 03 ==========')
+
+n1 = input('Digíte um número: ')
+n2 = input('Digíte outro número: ')
+soma = int(n1) + int(n2)
+
+print('A soma dos números inseridos é: ', soma)
